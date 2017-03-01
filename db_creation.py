79581ba541ac30c69b588b854e6887774694c490
Frank Yeh <frank.yeh25@myhunter.cuny.edu>
2017-03-01 11:06:10 -0500

    Updated and Works

--- db_creation.py
+++ db_creation.py
@@ -11,13 +11,21 @@ with con:
 
     cur = con.cursor()
     
-    done = int(input("Are you done? 1=y/0=n "))
-    while done != "1":
+    counter = int(input("How many students are you entering today? "))
+    while counter > 0:
         s_id = int(raw_input("What is the student ID? "))
         s_name = raw_input("What is the student name? ")
-        print(s_name)
         s_major = raw_input("What is the student major? ")
         s_career = raw_input("What is the student career? ")
-        print(s_id, " ", s_name, " ", s_major, " ", s_career)
-        cur.execute("INSERT INTO Student VALUES(s_id, s_name, s_major, s_career)")
-        done = int(input("Are you done? 1=y/0=n"))
+        params = (s_id, s_name, s_major, s_career)
+        cur.execute("INSERT INTO Student VALUES(?,?,?,?)", params)
+        con.commit()
+        counter = counter -1
+
+    tablecur = cur.execute("SELECT * FROM Student")
+    for row in tablecur:
+        print "Student ID = ", row[0]
+        print "Student Name = ", row[1]
+        print "Student Major = ", row[2]
+        print "Student Proposed Career = ", row[3]
+    
