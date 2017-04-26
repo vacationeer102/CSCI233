dfedf9901dec421e81533948d9bc6e4cb4e821a9
Frank Yeh <frank.yeh25@myhunter.cuny.edu>
2017-04-26 11:35:38 -0400

    asdf

--- Version 1.0.py
+++ Version 1.0.py
@@ -7,68 +7,129 @@ import os
 
 
 class Main_Window(Frame):
+    
     def createWidgets(self):
+        rowno= 0
+        
         self.s_id_l = Label(self, text= "Enter Student ID:")
-        self.s_id_l.pack(side= "left")
+        self.s_id_l.grid(row= rowno, column= 0)
 
         self.s_id_e = Entry(self)
-        self.s_id_e.pack(side= "left")
+        self.s_id_e.grid(row= rowno, column= 1)
 
         self.s_id_b = Button(self, text= "Search", command= self.search)
-        self.s_id_b.pack(side= "left")
+        self.s_id_b.grid(row= rowno, column= 2)
 
         self.QUIT = Button(self, text= "QUIT", command= self.quit)
-        self.QUIT.pack(side= "left")
+        self.QUIT.grid(row= rowno, column= 3)
 
         self.restart = Button(self, text= "RESTART", command= self.restart_program)
-        self.restart.pack(side= "left")
+        self.restart.grid(row= rowno, column= 4)
 
+        rowno= 1
 
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.pack()
-        self.createWidgets()
+        global rowno
+        
 
+    def blankLabel(self):
+        global rowno
+        self.blank = Label(self, text= " ")
+        self.blank.grid(row= rowno)
+        rowno= rowno+1
+        global rowno
+        
 
     def search(self):
         s_id = self.s_id_e.get()
         con = sql.connect('test.db')
 
+        global rowno
+        print rowno
+        
+        self.blankLabel()
+
         with con:
 
             s_cur = con.cursor()
             r_e_cur = con.cursor()
+
+            self.student_l = Label(self, text= "Student ID")
+            self.student_l.grid(row=rowno, column=0)
+            
+            self.student_l = Label(self, text= "Student Name")
+            self.student_l.grid(row=rowno, column=1)
+
+            self.student_l = Label(self, text= "Proposed Major")
+            self.student_l.grid(row=rowno, column=2)
+
+            self.student_l = Label(self, text= "Proposed Career")
+            self.student_l.grid(row=rowno, column=3)
+
+            rowno= rowno+1
             
             s_tablecur = s_cur.execute("SELECT * FROM Student WHERE student_id =?", (s_id,))
             for s_row in s_tablecur:
-                student = "Student ID = ", s_row[0], "Student Name = ", s_row[1], "Student Major = ", s_row[2], "Student Proposed Career = ", s_row[3]
-                self.student_l = Label(self, text= student)
-                self.student_l.pack(expand= 1, side= "bottom")
-                
-                r_e_temp = r_e_cur.execute("SELECT COUNT(r_e_name) FROM Recs_Evals WHERE r_e_id=?", (s_id,))
-                for row in r_e_temp:
-                    r_e_size=row[0]
+                for i in range(len(s_row)):
+                    self.student_l = Label(self, text= s_row[i])
+                    self.student_l.grid(row= rowno, column= i)
+            rowno= rowno+1
 
-                r_e_tablecur = r_e_cur.execute("SELECT * FROM Recs_Evals WHERE r_e_id=?", (s_id,))
-                r_e_list = [[None for y in range(4)] for x in range(r_e_size)]
-                i= 0
-                for r_e_row in r_e_tablecur:
-                    recommender = "Recommender Name = ", r_e_row[1], "Rec or Eval? ", r_e_row[2], "Date of Submission", r_e_row[3], "Relationship to Student = ", r_e_row[4]
-                    r_e_list[i][0] = r_e_row[1]
-                    r_e_list[i][1] = r_e_row[2]
-                    r_e_list[i][2] = r_e_row[3]
-                    r_e_list[i][3] = r_e_row[4]
-                    i = i+1
-                    self.recommender_l = Label(self, text= recommender)
-                    self.recommender_l.pack(expand= 1, side= "bottom")
+            self.blankLabel()
 
-                for j in range (0, len(r_e_list)):
-                    print r_e_list[j]
+            r_e_temp = r_e_cur.execute("SELECT COUNT(r_e_name) FROM Recs_Evals WHERE r_e_id=?", (s_id,))
+            for row in r_e_temp:
+                r_e_size= row[0]
+                self.recom_amount = Label(self, text= "Number of Recs and Evals")
+                self.recom_amount.grid(row= rowno, sticky= 'n')
+
+                self.recom_amount = Label(self, text= row[0])
+                self.recom_amount.grid(row= rowno, column= 1, sticky= 'n')
+            rowno= rowno+1
+
+            self.blankLabel()
+
+            r_e_tablecur = r_e_cur.execute("SELECT * FROM Recs_Evals WHERE r_e_id=?", (s_id,))
+            r_e_list = [[None for y in range(4)] for x in range(r_e_size)]
+            i=0
+            for r_e_row in r_e_tablecur:
+                r_e_list[i][0] = r_e_row[1]
+                r_e_list[i][1] = r_e_row[2]
+                r_e_list[i][2] = r_e_row[3]
+                r_e_list[i][3] = r_e_row[4]
+                i=i+1
+
+            self.recommender_l = Label(self, text= "Professor's Name")
+            self.recommender_l.grid(row= rowno, column= 0)
+
+            self.recommender_l = Label(self, text= "Recom? or Eval?")
+            self.recommender_l.grid(row= rowno, column= 1)
+
+            self.recommender_l = Label(self, text= "Time of Submission")
+            self.recommender_l.grid(row= rowno, column= 2)
+
+            self.recommender_l = Label(self, text= "Relationship")
+            self.recommender_l.grid(row= rowno, column= 3)
+
+            rowno= rowno+1
+
+            
+            for j in range (len(r_e_list)):
+                for k in range (4):
+                    self.recommender_l = Label(self, text= r_e_list[j][k])
+                    self.recommender_l.grid(row= j+rowno, column= k)
+
 
     def restart_program(self):
-        python = sys.executable
-        os.execl(python, python, * sys.argv)
-    
+        self.student_l.grid_remove()
+        self.recom_amount.grid_remove()
+        self.recommender_l.grid_remove()
+        
+        
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+                        
 root = Tk()
 app = Main_Window(master=root)
 app.mainloop()
